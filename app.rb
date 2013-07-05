a5ee39b332c14c3a90300f9895a6877c779a3db7
cieux1 <cieux1@gmail.com>
2013-07-05 21:27:24 +0900

    initial

---
+++ app.rb
@@ -0,0 +1,63 @@
+# -*- encoding: utf-8 -*-
+require 'rubygems'
+require 'sinatra'
+require 'anemone'
+require './page_inspector'
+
+def collected_pages(site_url)
+  
+  opts = {
+  :user_agent => "TestRobot/0.00",
+  :discard_page_bodies => true,
+  :depth_limit => 3,
+  :redirect_limit => 3,
+  :read_timeout => 3
+  }
+
+  arr = []
+  Anemone.crawl(site_url, options = opts) do |anemone|
+    anemone.on_every_page do |page|
+      arr << page.url.to_s
+    end
+  end
+  arr
+end
+
+def get_meta(pages)
+  puts "fetching...\n\n"
+  fetch_meta_from_all(pages)
+end
+
+def check_url(u)
+  if u == "http://" || u.empty?
+    raise "invalid url"
+  else
+    begin
+      bname = File.basename(u)
+      u = u.gsub(bname, "") if /html|shtml|php|jsp/ =~ bname
+      u = u.gsub(/^(http\:\/\/){2}/, "http://")
+      u = u.gsub(/\/+$/, "")
+    rescue => e
+      "Invalid URL(#{u}): #{e}"
+    end
+  end
+end
+
+
+get "/" do
+  @title = "metaExport2csv"
+  erb :index
+end
+
+get '/crawl' do
+  begin
+    @domain = check_url(params[:site_url])
+    @site = "#{@domain}/"
+    csv_name = "#{@domain}-#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}.csv"
+    content_type 'text/csv'
+    attachment csv_name
+    @site_data = get_meta(collected_pages(@site))
+  rescue => @e
+    erb :crawl_error
+  end
+end
---
+++ page_inspector.rb
@@ -0,0 +1,126 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+class Page
+  attr_accessor :url
+
+  def initialize(url)
+    @doc ||= Nokogiri::HTML(open(url)) rescue nil
+    @url = url
+  end
+  
+  def title
+    if @doc
+      title_txt ||= @doc.css('title').text.gsub(/\t|\n|\r/, '')
+      title_txt.empty? ? " " : title_txt
+      title_txt.gsub(/\,\t|\n|\r/, ' ')
+    else
+      title_txt = "404"
+    end
+  end
+  
+  def description
+    begin
+      desc ||= @doc.at('meta[@name="Description"]')['content']
+    rescue
+      begin
+        desc ||= @doc.at('meta[@name="description"]')['content']
+      rescue
+        desc ||= " "
+      end
+    end
+    desc.gsub(/\,\t|\n|\r/, ' ')
+  end
+  
+  def keywords
+    begin
+      keywords ||= @doc.at('meta[@name="keywords"]')['content']
+    rescue
+      begin
+        keywords ||= @doc.at('meta[@name="Keywords"]')['content']
+      rescue
+        keywords ||= " - "
+      end
+    end
+    keywords.gsub(",","-")
+  end
+  
+  def header_one
+    h_one ||= @doc.at('h1').inner_html rescue nil
+    h_one.gsub(/\t|\n|\r/, '') unless h_one.nil?
+  end
+  
+  def header_two
+    h_two = []
+    (@doc/"h2").each {|d| h_two << d.inner_html}
+    h_two.empty? ? [' '] : h_two
+    h_two.join(",").gsub(/\t|\n|\r/, '')   
+  end
+  
+  def redirect_page?
+    /http-equiv=\"refresh\"/ =~ @doc.to_s.downcase
+  end
+
+  def redirect
+    "redirect" if redirect_page?
+  end
+
+  def canonical_url
+    if /<link rel\=\"canonical\" href\=\"(.*?)\"/ =~ @doc.to_s.downcase
+      clean($1)
+    else
+      " "
+    end
+  end
+
+  def noindex_page?
+    /content=\"\w*(,\s?)?noindex\"/ =~ @doc.to_s.downcase
+  end
+
+  def noindex
+    "noindex" if noindex_page?
+  end
+
+  def nofollow_page?
+    /content=\"\w*(,\s?)?nofollow\"/ =~ @doc.to_s.downcase
+  end
+
+  def nofollow
+    "nofollow" if noindex_page?
+  end
+
+  def clean(u)
+    u.gsub("index\.html", "").gsub("http\:\/\/", "")
+  end
+
+  def print_less
+    "#{clean(@url)}, #{redirect}\n\r"
+  end
+
+  def print_all
+    "#{clean(@url)}, #{redirect}, #{noindex}, #{nofollow}, #{canonical_url}, #{title}, #{description}, #{keywords}, #{header_one}, #{header_two}\n\r"
+  end
+
+end
+
+def print_header
+  "url, redirect, noindex, nofollow, canonical, title, meta description, keywords, h1, h2\n\r"
+end
+
+def fetch_meta_from_all(pages)
+  str = print_header
+  pages.each do |page|
+    begin
+      url = Page.new(page)
+      if url.redirect_page?
+        str << url.print_less
+      else
+        str << url.print_all
+      end
+    rescue => e
+      str << "#{page},URL not found ? \n\r"
+    end
+  end
+  str
+end
